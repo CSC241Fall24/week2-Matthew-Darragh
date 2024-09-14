75faed80c2f7b231715f5aadddeb3e4e762870a4
Matthew Darragh <MD956670@wcupa.edu>
2024-09-13 20:20:22 -0400

    2.1

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -1,35 +1,33 @@
 // src/main/java/Q1WeirdFunction.java
-
 public class Q1WeirdFunction {
 
     public static int fRecursive(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException("Input must be non-negative");
-        }
-        if (n < 3) {
+        if (n < 3)
+        {
             return n;
         }
-        return fRecursive(n - 1) + 2 * fRecursive(n - 2) + 3 * fRecursive(n - 3);
+        if(n >= 3)
+        {
+            return fRecursive(n - 1) + 2 * fRecursive(n - 2) + 3 * fRecursive(n - 3);
+        }
+
     }
 
     public static int fIterative(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException("Input must be non-negative");
-        }
         if (n < 3) {
             return n;
         }
         
-        int f0 = 0, f1 = 1, f2 = 2;
-        int result = 0;
+        int[] f = new int[n + 1];
+        f[0] = 0;
+        f[1] = 1;
+        f[2] = 2;
         
         for (int i = 3; i <= n; i++) {
-            result = f2 + 2 * f1 + 3 * f0;
-            f0 = f1;
-            f1 = f2;
-            f2 = result;
+            f[i] = f[i - 1] + 2 * f[i - 2] + 3 * f[i - 3];
         }
         
-        return result;
+        return f[n];
     }
+
 }
